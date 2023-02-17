1e88eb71150219531e3ce2c7dac8652bab143671
SergioRod <sergio.rod.perez@outlook.es>
2023-02-17 18:01:23 -0500

    ngFor y get for api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hello-app';
+  http = inject(HttpClient);
 
   changeTitle(){
     this.title = "Sergio"
